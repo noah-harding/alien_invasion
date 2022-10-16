c7b3713933eaa9a9f10ea4803f39e825b585d1ec
noah-harding <m252328@usna.edu>
2022-10-16 12:21:46 -0400

    added official ship

--- ship.py
+++ ship.py
@@ -7,10 +7,10 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load("red_spaceship.bmp")
+        self.image = pygame.image.load("images/grey_spaceship.bmp")
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
 
-    def bliteme(self):
+    def blitme(self):
         self.screen.blit(self.image, self.rect)
